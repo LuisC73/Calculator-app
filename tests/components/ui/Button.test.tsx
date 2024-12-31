1f8e66e4465923a4540a205efa44610080f3c256
Miguel Castro <luiscastroc11@gmail.com>
2024-12-31 16:35:01 -0500

    Chore: add jest and testing library

---
+++ tests/components/ui/Button.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Button } from '../../../src/components';
+
+describe('Firts', () => {
+  test('should first', () => {
+    render(<Button />);
+  });
+});
