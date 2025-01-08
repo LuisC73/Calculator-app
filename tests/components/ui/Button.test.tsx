3df35cd8bfb83be8d0cae03dc0a4c9a0e0ac9edd
Miguel Castro <luiscastroc11@gmail.com>
2025-01-08 17:48:35 -0500

    Feat: add first test

--- tests/components/ui/Button.test.tsx
+++ tests/components/ui/Button.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { describe, it } from 'vitest';
-import { render } from '@testing-library/react';
+import { render } from '../../test-utils';
+import { screen } from '@testing-library/react';
+import { Button } from '../../../src/components';
 
-describe('first', () => {
+describe('tests in component <Button />', () => {
   it('first test', () => {
-    render(<p>hello</p>);
+    render(<Button id="number-7" label="7" type="primary" action={() => {}} />);
+    screen.debug();
   });
 });
